a520e8c90c583e0520dd34b4f822546029680419
ayuushpokharel <ayuushpokharel@gmail.com>
2026-05-18 17:59:07 +0545

    jwt utils

---
+++ src/utils/jwt.utils.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import { Role } from "../types/enum.types";
+import mongoose from "mongoose";
+import ENV_CONFIG from "../config/env.config";
+
+type TPayLoad = {
+    _id: mongoose.Schema.Types.ObjectId;
+    full_name?: string;
+    role: Role;
+    email: string;
+};
+//!
+export const generateToken = (payLoad: TPayLoad) => {
+    try {
+        const access_token = jwt.sign(payLoad, ENV_CONFIG.jwt_secret, {
+            expiresIn: ENV_CONFIG.jwt_expiry as any,
+        });
+    } catch (error: any) {
+        console.log(error);
+        throw error;
+    }
+};
