ed3f7d37c8fb7e9a2efa639e67385b42dfde04ec
ayuushpokharel <ayuushpokharel@gmail.com>
2026-05-26 15:39:39 +0545

    refactor: simplify Request interface by replacing user object with TPayLoad type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,15 +1,9 @@
-import mongoose from "mongoose";
 import TPayLoad from "../utils/jwt.utils";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: {
-        _id: mongoose.Types.ObjectId;
-        full_name?: string;
-        role: Role;
-        email: string;
-      };
+      user?: TPayLoad;
       file?: Multer.File;
       files?: Multer.File[];
     }
