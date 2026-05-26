7893c6ee5c76b5decd1e843bf8ebe709728f5350
ayuushpokharel <ayuushpokharel@gmail.com>
2026-05-26 15:36:41 +0545

    refactor: update Request interface to define user object structure

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,15 @@
-import { TPayLoad } from "../utils/jwt.utils";
+import mongoose from "mongoose";
+import TPayLoad from "../utils/jwt.utils";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: TPayLoad;
+      user?: {
+        _id: mongoose.Types.ObjectId;
+        full_name?: string;
+        role: Role;
+        email: string;
+      };
       file?: Multer.File;
       files?: Multer.File[];
     }
